e31b08bc243a14bef90769e03cbe4949d59106ab
Cyril Boillat <35902374+SiropOps@users.noreply.github.com>
2026-05-18 18:48:47 +0200

    Add Material Components dependency for Material3 theme resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
